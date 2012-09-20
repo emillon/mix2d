6fc38a9943b92147910e21c3c6c3b35a23c5f67e
Etienne Millon <me@emillon.org>
2012-09-20 09:05:33 +0200

    draw contexts instead of colors

--- mix2d.js
+++ mix2d.js
@@ -1,21 +1,15 @@
-function mix2d_draw(id, f) {
-
-  var g1 = [100, 100, 100];
-  var g2 = [120, 120, 120];
-  var g3 = [140, 140, 140];
-
-  var colors = [g1, g2, g3];
+function mix2d_draw(id, sources, f) {
   var canvas = document.getElementById(id);
   var ctx = canvas.getContext("2d");
   var data = ctx.getImageData(0, 0, canvas.width, canvas.height);
   var i, j, p;
   for(i=0;i<canvas.width;i++) {
     for(j=0;j<canvas.height;j++) {
-      var col = colors[~~(f(i, j)) % colors.length];
+      var source = sources[~~(f(i, j)) % sources.length];
       p = 4*(j*canvas.width+i);
-      data.data[p+0] = col[0];
-      data.data[p+1] = col[1];
-      data.data[p+2] = col[2];
+      data.data[p+0] = source.data[p+0];
+      data.data[p+1] = source.data[p+1];
+      data.data[p+2] = source.data[p+2];
       data.data[p+3] = 255;
     }             
   }
