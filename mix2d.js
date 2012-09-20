fa22fc96c517163f2914093832dff0c78fff7c90
Etienne Millon <me@emillon.org>
2012-09-20 09:53:02 +0200

    Add image loader

--- mix2d.js
+++ mix2d.js
@@ -15,3 +15,34 @@ function mix2d_draw(id, sources, f) {
   }
   ctx.putImageData(data, 0, 0);
 }
+
+function makeContext(w, h) {
+  var canvas = document.createElement('canvas');
+  canvas.height = h;
+  canvas.width  = w;
+  return canvas.getContext("2d");
+}
+
+function fromColor(color) {
+  var h = 512;
+  var w = 512;
+  var ctx = makeContext(w, h);
+  ctx.fillStyle = color;
+  ctx.fillRect(0, 0, w, h);
+  var data = ctx.getImageData(0, 0, w, h);
+  return data;
+}
+
+function fromImage(src, k) {
+  var h = 512;
+  var w = 512;
+  var img = new Image();
+  img.onload = function () {
+    var ctx = makeContext(w, h);
+    ctx.drawImage (img, 0, 0);
+    var data = ctx.getImageData(0, 0, w, h);
+    console.log(data);
+    k(data);
+  };
+  img.src = src;
+}
